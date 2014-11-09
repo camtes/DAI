d7c1de3485b367a35d4793f292a7f07b43763c7c
Carlos Campos <carlos@ccamposfuentes.es>
2014-11-09 18:12:02 +0100

    update

--- p3/mako2.py
+++ p3/mako2.py
@@ -7,17 +7,26 @@ import web
 from web import form
 from web.contrib.template import render_mako
 
+# Para poder usar sesiones con web.py
 web.config.debug = False
 
-urls = ( '/', 'index' )
+# Paginas enlazadas
+urls = ( '/', 'index',
+        '/logout', 'logout' )
 
 app = web.application(urls, globals(), autoreload=True)
+
+# Inicializamos la variable session a cadena vacía porque inicialmente no hay ningún usuario que haya iniciado sesion
+session = web.session.Session(app, web.session.DiskStore('sessions'))
+
+# Formulario para hacer login
 formulario = form.Form(
 	form.Textbox('usuario', form.notnull, maxlenght="30", description="Usuario: "),
 	form.Password('password', maxlenght="10", description="Contraseña: "),
 	form.Button("Login")
 	)
 
+# Plantilla de mako
 plantillas = render_mako(
         directories=['templates'],
         input_encoding='utf-8',
@@ -25,29 +34,25 @@ plantillas = render_mako(
         )
 
 class index:
-    def GET(self):
-        form = formulario()
-        return plantillas.index(formu = form.render())
+  def GET(self):
+    formLogin = formulario()
+    return plantillas.index(form = formLogin)
 
-    def POST(self):
-        usuario = web.input()
-        user = usuario.nombre
-        return plantillas.index(formu = "Bienvenido/a ".user)
+  def POST(self):
+    formLogin = formulario()
 
+    if not formLogin.validates():
+      return plantillas.index(form = formLogin)
 
-	#def POST(self): #METER ESTO A MANO
-	#	formu = formulario()
-#
- #   	if not formu.validates():
-  #  		return plantillas.mako(formu = formu.render())
-   # 	
-    #    else:
-     #   	usuario = web.input()
-      #  	usu_nombre = usuario.usuario
-       # 	ses.nombre = usu_nombre
-#
- #       	return plantillas.mako(formu = ses.nombre+" (logout)")
-			
+    else:
+      i = web.input()
+      usuario = i.usuario
+      session.usuario = usuario
+      return plantillas.index(mensaje = "Bienvenido " + str(session.usuario) + "(<a href='/logout'>Logout</a>)")
+
+class logout:
+  session.usuario = False
+  raise web.seeother('/')
 
 if __name__ == "__main__":
     app.run()
